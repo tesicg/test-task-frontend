d1b97af1cf1d41f3694d556f22b9632ecb592735
Goran Teshich <goranvostok@gmail.com>
2025-05-25 11:07:49 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SelectAndDisplay from './components/SelectAndDisplay.vue'
+</script>
+
+<template>
+    <select-and-display />
+</template>
+
+<style scoped></style>
---
+++ src/components/TopLeft.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="list" v-if="selectedItems.length > 0">
+        <div class="box" v-for="item in selectedItems" :key="item.id">
+            <div>{{ item.name }}</div>
+        </div>
+    </div>
+    <div v-else class="empty-message">Select 1-6 items from the bottom left</div>
+</template>
+
+<script setup>
+defineProps({
+    selectedItems: {
+        type: Array,
+        default: () => [],
+        required: true,
+    },
+})
+</script>
+
+<style lang="scss" scoped></style>
